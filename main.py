d2824d15410726397fb174279bc73393b1a10c11
ericgcc <ericgcc@gmail.com>
2020-01-13 12:00:52 -0500

    [core] Adding first 8 stops
    The programming of the first 8 stops of the route was added, controlling the robot through the functions drive_straight and turn_in_place

--- main.py
+++ main.py
@@ -11,6 +11,41 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
     m = CustomMap(robot)
 
+    # Stop 1
+    robot.turn_in_place(degrees(-5)).wait_for_completed()
+    robot.drive_straight(distance_mm(200), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(-85)).wait_for_completed()
+
+    # Stop 2
+    robot.drive_straight(distance_mm(115), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(90)).wait_for_completed()
+
+    # Stop 3
+    robot.drive_straight(distance_mm(275), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(90)).wait_for_completed()
+
+    # Stop 4
+    robot.drive_straight(distance_mm(105), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+
+    # Stop 5
+    robot.drive_straight(distance_mm(250), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(180)).wait_for_completed()
+
+    # Stop 6
+    robot.drive_straight(distance_mm(220), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(90)).wait_for_completed()
+
+    # Stop 7
+    robot.drive_straight(distance_mm(130), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(-45)).wait_for_completed()
+    robot.drive_straight(distance_mm(135), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(-35)).wait_for_completed()
+
+    # Stop 8
+    robot.drive_straight(distance_mm(110), speed_mmps(50)).wait_for_completed()
+    robot.turn_in_place(degrees(85)).wait_for_completed()
+
     while True:
         time.sleep(1)
 
