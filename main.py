9decb1a59441c1591ec512236d1415039ccf0458
ericgcc <ericgcc@gmail.com>
2020-01-23 11:01:09 -0500

    [core] Removing unnecessary files and creation of main program
    Files used in other courses were removed. The instructions to move the robot across the labyrinth were created,

--- main.py
+++ main.py
@@ -2,49 +2,67 @@ import time
 import cozmo
 from cozmo.objects import CustomObject
 from cozmo.util import degrees, distance_mm, speed_mmps
-from custom_map import CustomMap
 
 def cozmo_program(robot: cozmo.robot.Robot):
     # réinitialisation des objets
     robot.world.delete_all_custom_objects()
     print(robot.pose.position)
 
-    m = CustomMap(robot)
-
-    # Stop 1
-    robot.turn_in_place(degrees(-5)).wait_for_completed()
-    robot.drive_straight(distance_mm(200), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(-85)).wait_for_completed()
-
-    # Stop 2
-    robot.drive_straight(distance_mm(115), speed_mmps(50)).wait_for_completed()
+    # Turn 1
     robot.turn_in_place(degrees(90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
 
-    # Stop 3
-    robot.drive_straight(distance_mm(275), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(90)).wait_for_completed()
-
-    # Stop 4
-    robot.drive_straight(distance_mm(105), speed_mmps(50)).wait_for_completed()
+    # Turn 2
     robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
 
-    # Stop 5
-    robot.drive_straight(distance_mm(250), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(180)).wait_for_completed()
+    # Turn 3-4
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(900), speed_mmps(50), should_play_anim=True).wait_for_completed()
 
-    # Stop 6
-    robot.drive_straight(distance_mm(220), speed_mmps(50)).wait_for_completed()
+    # Turn 5
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 6
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(600), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 7-9
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(900), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 10
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(600), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 11-12
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(600), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 13-14
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(900), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 15-16
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(600), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 17
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 18
+    robot.turn_in_place(degrees(-90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
+
+    # Turn 19
     robot.turn_in_place(degrees(90)).wait_for_completed()
+    robot.drive_straight(distance_mm(300), speed_mmps(50), should_play_anim=True).wait_for_completed()
 
-    # Stop 7
-    robot.drive_straight(distance_mm(130), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(-45)).wait_for_completed()
-    robot.drive_straight(distance_mm(135), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(-35)).wait_for_completed()
-
-    # Stop 8
-    robot.drive_straight(distance_mm(110), speed_mmps(50)).wait_for_completed()
-    robot.turn_in_place(degrees(85)).wait_for_completed()
+    # Turn 19
+    robot.turn_in_place(degrees(90)).wait_for_completed()
+    robot.drive_straight(distance_mm(350), speed_mmps(50), should_play_anim=True).wait_for_completed()
 
     while True:
         time.sleep(1)
